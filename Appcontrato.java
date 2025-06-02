8dd1567eab06acc30e7a4318dcba72fb2d7e75ec
DPaulaa <bd.pauladias@gmail.com>
2025-06-02 18:46:08 -0300

    primeiro commit

---
+++ Appcontrato.java
@@ -0,0 +1,166 @@
+/*
+pedro, seguinte, te informar o que não consegui fazer. não consegui fazer da 3 a 5 (buscar, aditar e excluir) e toda
+ vez que começa um novo processo, ele cria um novo .txt e substitui o ja existente. o crud não fiz, porque não entendi muito.
+*/
+
+
+
+
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+public class Appcontrato {
+
+    public static void main(String[] args) {
+        
+        String contratante, contratado, nContrato, desc, cpf, celebracao, inicio, termino;
+        Double valcontrato;
+        int opcao, ID=0;
+        boolean fim1 = false; //variavel para declarar fim a opção 1 e inicio a outras opções 
+        Scanner ler = new Scanner (System.in);
+        ArrayList<contrato> meuContrato = new ArrayList<>();
+        
+        
+        
+        
+        
+        
+        do {
+        System.out.println("");
+        System.out.println("Opções para operações: ");
+        System.out.println("1. Criar novo registro ");
+        System.out.println("2. Listar todos os registros ");
+        System.out.println("3. Buscar registros (por identificador) ");
+        System.out.println("4. Editar registro ");
+        System.out.println("5. Excluir registro ");
+        System.out.println("6. Sair ");
+        opcao = ler.nextInt();
+        ler.nextLine();
+        System.out.println("");
+        
+        
+        
+        
+        
+        
+        if (opcao == 1){
+            fim1 = true;
+            ID++; //ID auto incrementavel
+        System.out.println("Digite o nome do contratante: ");
+        contratante = ler.nextLine();
+             
+        System.out.println("Digite o nome do contratado: ");
+        contratado = ler.nextLine();
+        System.out.println("Digite o numero do contrato: ");
+        nContrato = ler.nextLine();
+        System.out.println("Digite a descricao do contrato: ");
+        desc = ler.nextLine();
+        System.out.println("Digite o CPF do contratante: ");
+        cpf = ler.nextLine();
+        System.out.println("Digite a data de celebracao do contrato: ");
+        celebracao = ler.nextLine();
+        System.out.println("Digite a data de inicio do contrato: ");
+        inicio = ler.nextLine();
+        System.out.println("Digite a data de termino do contrato: ");
+        termino = ler.next();
+        System.out.println("Digite o valor do contrato: ");
+        valcontrato = ler.nextDouble();
+        ler.nextLine();
+        
+        contrato cont = new contrato(ID, contratante, contratado, nContrato, desc, valcontrato, celebracao, inicio, termino, cpf);
+     
+        //se o CPF for invalido, conforme classe, retornara para o app e solicitara um valido
+        while (!ValidaCPF.validarCPF(cpf)){
+        System.out.println("CPF invalido!");
+        System.out.println("Digite o CPF do contratante: ");
+        cpf = ler.next(); 
+        }
+        //se as datas forem invalidas
+        while (cont.validaCelebrar() && cont.validarInicio() && cont.validarTermino()){
+            
+            
+            System.out.println("Digite a data de celebracao do contrato: ");
+            celebracao = ler.next();
+            System.out.println("Digite a data de inicio do contrato: ");
+            inicio = ler.next();
+            System.out.println("Digite a data de termino do contrato: ");
+            termino = ler.next();
+            
+        }
+        
+            //adiciona os dados e cria um arquivo .txt        
+        contrato.class.getDeclaredMethods();
+        meuContrato.add (new contrato(ID, contratante, contratado, nContrato, desc, valcontrato, celebracao, inicio, termino, cpf));
+        
+        
+        
+        
+        try{
+            try (FileWriter escritor = new FileWriter("contrato.txt")) {
+                for (contrato c : meuContrato) {
+                    escritor.write(c.toString() +"\n");
+                }
+            }
+        System.out.println("Registro criado com sucesso!");
+        } catch (IOException e){
+            System.out.println("Erro ao criar registro!");
+        }
+        
+        
+        //opção 2, caso exista um arquivo txt
+        } else if (fim1 ==true && opcao == 2) {
+            try{            
+                try (FileReader leitor = new FileReader("contrato.txt"); 
+                BufferedReader buffer = new BufferedReader(leitor)) {
+                    
+                    String linha;
+                    
+                    while ((linha = buffer.readLine()) !=null){
+                        System.out.println("Frase lida: " +linha);
+                    }
+                }
+            } catch(IOException e){
+                System.out.println("Erro ao ler o arquivo!");
+            }
+            
+        }
+        
+        //opção 2, caso não exista um arquivo txt
+        else if (opcao == 2 && fim1 == false){
+        try{            
+                try (FileReader leitor = new FileReader("contrato.txt"); 
+                BufferedReader buffer = new BufferedReader(leitor)) {
+                    
+                    String linha;
+                    
+                    while ((linha = buffer.readLine()) !=null){
+                        System.out.println("Frase lida: " +linha);
+                    }
+                }
+            } catch(IOException e){
+                System.out.println("Erro ao ler o arquivo!");
+            }
+            
+        
+        }else if (opcao == 3){
+            
+            
+        }
+        
+        //finaliza o processo
+        } while(opcao !=6);
+        System.out.println("kbo");
+        
+       
+        
+        
+    }
+
+    
+}
---
+++ contrato.java
@@ -0,0 +1,128 @@
+
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+
+
+public class contrato {
+    private int ID;
+    private String contratante;
+    private String contratado;
+    private String ncontrato;
+    private String desccontrato;
+    private Double valcontrato;
+    private LocalDate celebracao;
+    private LocalDate inicio;
+    private LocalDate termino;
+    private String CPF;
+    private String valcontratoBR;
+    
+    
+        
+    //classe responsavel especificar um padrão diferente do formato ISO
+    DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    
+    //metodo responsavel para formatar uma data recebida para o "formato"
+    private LocalDate validarData(String dataString){
+        return LocalDate.parse(dataString, formato);
+    }
+    
+    //objeto criado para padronizar o formato da moeda para pt-BR
+    Locale localBrasil = new Locale("pt", "BR");
+    
+    
+    //construtor
+    public contrato (int ID, String contratante, String contratado, String ncontrato, String desccontrato, Double valcontrato, String celebracao, String inicio, String termino, String CPF){
+        this.celebracao = validarData(celebracao);
+        this.inicio = LocalDate.parse(inicio, formato);
+        this.termino = LocalDate.parse(termino, formato);
+        this.contratado = contratado;
+        this.contratante = contratante;
+        this.desccontrato = desccontrato;
+        this.ncontrato = ncontrato;
+        this.valcontratoBR = NumberFormat.getCurrencyInstance(localBrasil).format(valcontrato);
+        this.CPF = ValidaCPF.imprimeCPF(CPF) ; //padronizar cpf
+        this.ID = ID;
+    }
+    
+     
+
+    
+    //metodo para validar se a data é maior ou menor que outra data
+    
+    
+    
+     public boolean validaCelebrar(){
+         if (celebracao.isAfter(termino)){
+             System.out.println("Data de celebracao não pode ser posterior a data de termino!");
+             return true;
+         }else {
+        return false; 
+         }
+        }
+     
+     public boolean validarInicio (){
+         if (inicio.isAfter(termino)){
+             System.out.println("Data de inicio não pode ser posterior a data de termino!");
+         return true;
+         }else {
+        return false; 
+         }
+        }
+     
+     public boolean validarTermino (){
+         if (termino.isBefore(celebracao) && termino.isBefore(inicio)){
+             System.out.println("Data de termino não pode ser anterior a data de celebracao e inicio!");
+         return true;
+         }else {
+        return false; 
+         }
+        }
+    //metodo getters para acessar atributos privados
+    
+    public String getCelebracaoFormato(){
+        return celebracao.format(formato);
+    }
+    
+    public String getInicio(){
+        return formato.format(inicio);
+    }
+    
+    public String getTermino(){
+        return formato.format(termino);
+    }
+    
+    public String getContratante(){
+        return contratante;
+    }
+    
+    public String getContratado(){
+        return contratado;
+    }
+    
+    public String getNcontrato(){
+        return ncontrato;
+    }
+    
+    public String getDesccontrato(){
+        return desccontrato;
+    }
+    
+    public String getValcontratoBR(){
+        return valcontratoBR;
+    }
+    
+    public String getCPF(){
+        return CPF;
+    }
+    public int getID(){
+        return ID;
+    }
+    
+    public String toString(){
+    return ID +"|" +contratante + " | " + contratado + " | " + CPF + " | " + ncontrato + " | " + valcontratoBR + " | " + celebracao.format(formato) + 
+            " | " + formato.format(inicio) + " | " + formato.format(termino) + " | " + desccontrato;
+    }
+}
